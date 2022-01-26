c96013051050d5f56c7daaf420da3bf5183a9d7e
jaybhalani3011 <jaybhalani3011@gmail.com>
2022-01-26 16:25:01 +0530

    third commit

--- pages/docs/[...params].jsx
+++ pages/docs/[...params].jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 export default function docs() {
+    const router = useRouter();
+    const { params } = router.query;
+    console.log(params); //Found the array of all the nested params after docs... 
     return (
         <div>
             <h1>Here in docs with catch all routes.</h1>
