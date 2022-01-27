c87cf2b682c8163d7d09ef4358d00f8267933f42
jaybhalani3011 <jaybhalani3011@gmail.com>
2022-01-27 11:51:29 +0530

    commit of Link

--- pages/docs/[[...params]].jsx
+++ pages/docs/[[...params]].jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export default function docsAndDocsParams() {
     const router = useRouter();
@@ -7,6 +8,7 @@ export default function docsAndDocsParams() {
     console.log(params); //Found the array of all the nested params after docs... 
     return ( 
         <div>
+            <Link href='/'><a>Bach to home ⬅</a></Link>
             <h1>Here we are in master branch docs with catch all routes.</h1>
         </div>
     )
--- pages/post/index.jsx
+++ pages/post/index.jsx
@@ -1,9 +1,25 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function index() {
     return (
         <div>
             <h1>Here in post(index of post) route.</h1>
+            <h2>
+                <Link href={'/post/1'}>
+                    <a>post :- 1</a>
+                </Link>
+            </h2>
+            <h2>
+                <Link href={'/post/2'}>
+                    <a>post :- 2</a>
+                </Link>
+            </h2>
+            <h2>
+                <Link href={'/post/3'}>
+                    <a>post :- 3</a>
+                </Link>
+            </h2>
         </div>
     )
 }
