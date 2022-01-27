6e357daff0010d5eab01c99a1929ff433f8b1e5d
jaybhalani3011 <jaybhalani3011@gmail.com>
2022-01-27 10:57:56 +0530

    in master branch

--- pages/docs/[[...params]].jsx
+++ pages/docs/[[...params]].jsx
@@ -7,7 +7,7 @@ export default function docsAndDocsParams() {
     console.log(params); //Found the array of all the nested params after docs... 
     return ( 
         <div>
-            <h1>Here in docs with catch all routes.</h1>
+            <h1>Here we are in master branch docs with catch all routes.</h1>
         </div>
     )
 }
